0cb567ace41defc62a672b8f1b612379abaf5527
Anwesh43 <anweshthecool0@gmail.com>
2021-03-25 00:35:37 +0530

    adding constants and libraries

--- BiRectMidXView/src/main/java/com/example/birectmidxview/BiRectMidXView.kt
+++ BiRectMidXView/src/main/java/com/example/birectmidxview/BiRectMidXView.kt
@@ -1,2 +1,27 @@
 package com.example.birectmidxview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#2196F3",
+    "#00C853",
+    "#DD2C00"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val sizeFactor : Float = 2.9f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
+
